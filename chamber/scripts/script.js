9f96a07e0181165855e1ea811f7dda63daee293b
jacrue <ruesch0311@gmail.com>
2023-01-31 01:55:04 -0600

    Chambers Website

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,12 @@
 let text = document.lastModified;
 const date = new Date(document.lastModified);
 document.getElementById("updated").innerHTML = date;
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
