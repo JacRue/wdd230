4c1d41fa0d9511f79c67e84463f8b2164ab7e0c1
jacrue <ruesch0311@gmail.com>
2023-02-01 19:38:37 -0600

    Added date to header

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,10 +3,16 @@ const date = new Date(document.lastModified);
 document.getElementById("updated").innerHTML = date;
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+window.onload = function() {
+  var today = new Date();
+  var date = today.toDateString();
+  document.getElementById("date").innerHTML = date;
+};
